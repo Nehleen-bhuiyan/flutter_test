3a66ca29f68c9f5025ba3db76397de199404f3e1
Nehleen-bhuiyan <nehleenbhuiyan@gmail.com>
2026-08-09 21:09:58 +0600

    Refactor home_screen.dart to manage blood details
    Removed InterestCard class and related widgets. Added BloodDetails class and BloodCard widget to display blood request information.

--- first_flutter/lib/home_screen.dart
+++ first_flutter/lib/home_screen.dart
@@ -1,4 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:health_icons/health_icons.dart';
+
+class BloodDetails {
+  int? units;
+  String? bloodType;
+  String? hospital;
+  BloodDetails(this.bloodType, this.units, this.hospital);
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,266 +18,166 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class InterestCard {
-  String name;
-  String picture;
-  String desc;
-  InterestCard(this.name, this.picture, this.desc);
-}
-
 class _HomeScreenState extends State<HomeScreen> {
-  int selectedIndex = 0;
-  List<InterestCard> cards = [
-    InterestCard(
-      "Travel",
-      "assets/travel.jpg",
-      "Exploring new places around the world",
-    ),
-    InterestCard(
-      "Photography",
-      "assets/photography.jpg",
-      "Capturing moments through lenses",
-    ),
-  ];
-
   // This widget is the root of your application.
+  BloodDetails bloodDetails1 = BloodDetails(
+    "O-",
+    3,
+    'Khulna City Medical College Hospital,KDA avenue',
+  );
+  BloodDetails bloodDetails2 = BloodDetails(
+    "O+",
+    2,
+    'Khulna City Medical College Hospital,KDA avenue',
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blueGrey, title: Text("My app")),
       body: ListView(
+        padding: EdgeInsets.all(8),
         children: [
-          Padding(
-            padding: EdgeInsets.only(top: 30, left: 4, right: 4, bottom: 15),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(12),
-              ),
-              height: 455,
-              width: double.infinity,
-              padding: EdgeInsets.only(left: 30, right: 30, top: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: const Color.fromARGB(100, 113, 110, 103),
-                    backgroundImage: const AssetImage(
-                      'assets/flutter_example.jpg',
-                    ),
-                    radius: 80,
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    "Kanamori Sayaka",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5),
-                  Text("Flutter Developer"),
-                  SizedBox(height: 15),
-                  Text(
-                    "Passionate about creating user-friendly and engaging digital expreiences.",
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Icon(Icons.mail, color: Colors.blueGrey),
-                      SizedBox(width: 5),
-                      Text("sayaka_kanamori@gmail.com"),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Row(
-                    children: [
-                      Icon(Icons.phone, color: Colors.blueGrey),
-                      SizedBox(width: 5),
-                      Text("+123-456-789"),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 50,
-                        width: 125,
-                        child: FilledButton(
-                          onPressed: () {
-                            setState(() {
-                              selectedIndex = 0;
-                            });
-                          },
-
-                          style: FilledButton.styleFrom(
-                            backgroundColor: selectedIndex == 0
-                                ? Colors.blue
-                                : Colors.white,
-                            foregroundColor: selectedIndex == 0
-                                ? Colors.white
-                                : Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-
-                          child: const Text(
-                            "Follow",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      SizedBox(
-                        height: 50,
-                        width: 125,
-                        child: FilledButton(
-                          onPressed: () {
-                            setState(() {
-                              selectedIndex = 1;
-                            });
-                          },
-
-                          style: FilledButton.styleFrom(
-                            backgroundColor: selectedIndex == 1
-                                ? Colors.blue
-                                : Colors.white,
-                            foregroundColor: selectedIndex == 1
-                                ? Colors.white
-                                : Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-
-                          child: const Text("Message"),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+          Center(
+            child: Text(
+              "Blood Requests",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 122,
-                  height: 1,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.transparent, Colors.grey],
-                    ),
-                  ),
-                ),
-
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 12),
-                  child: Text(
-                    "Interests",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-
-                Container(
-                  width: 122,
-                  height: 1,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.grey, Colors.transparent],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InterestCardWidget(card: cards[0]),
-
-                const SizedBox(width: 15),
-
-                InterestCardWidget(card: cards[1]),
-              ],
-            ),
-          ),
+          SizedBox(height: 20),
+          BloodCard(bloodDetails: bloodDetails1),
+          SizedBox(height: 20),
+          BloodCard(bloodDetails: bloodDetails2),
         ],
       ),
     );
   }
 }
 
-class InterestCardWidget extends StatelessWidget {
-  const InterestCardWidget({super.key, required this.card});
+class BloodCard extends StatelessWidget {
+  const BloodCard({super.key, required this.bloodDetails});
 
-  final InterestCard card;
+  final BloodDetails bloodDetails;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 160,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.asset(
-              card.picture,
-              height: 100,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.only(left: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+            border: Border.all(color: Colors.grey.shade200),
           ),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 14,
+                      vertical: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color.fromARGB(120, 252, 215, 215),
+                    ),
+                    child: const Text(
+                      "URGENT",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
 
-          const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      const Icon(Icons.water_drop, color: Colors.red, size: 22),
+                      const SizedBox(width: 4),
+                      Text("${bloodDetails.units} Units"),
+                    ],
+                  ),
+                ],
+              ),
 
-          Text(
-            card.name,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
+              const SizedBox(height: 20),
 
-          const SizedBox(height: 5),
-
-          Text(
-            card.desc,
-            style: const TextStyle(fontSize: 13, color: Colors.grey),
-          ),
-
-          const SizedBox(height: 12),
-
-          SizedBox(
-            width: double.infinity,
-            child: FilledButton(
-              onPressed: () {
-                // static button for now
-              },
-              style: FilledButton.styleFrom(
-                backgroundColor: Colors.grey[200],
-                foregroundColor: Colors.blue,
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
+              Text(
+                "${bloodDetails.bloodType} Blood Needed",
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              child: const Text(
-                "View More",
-                style: TextStyle(fontWeight: FontWeight.bold),
+
+              const SizedBox(height: 20),
+
+              Row(
+                children: [
+                  Icon(Icons.location_on_outlined, color: Colors.grey[400]),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: Text(
+                      bloodDetails.hospital ?? '',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(199, 35, 35, 134),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text(
+                    "View Details",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        Positioned(
+          left: 10,
+          top: 2,
+          bottom: 2,
+          child: Container(
+            width: 10,
+
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
